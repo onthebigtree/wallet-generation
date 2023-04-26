e5faef2e75bd5d0e95f74c2ddb04f17e73d31ff2
Yewlne <60813639+onthebigtree@users.noreply.github.com>
2023-04-26 13:25:51 +0800

    新增后缀可选

--- main.py
+++ main.py
@@ -2,36 +2,41 @@ import os
 import threading
 from eth_account import Account
 
+# 创建钱包的函数，返回一个新钱包对象
 def create_wallet():
     wallet = Account.create(os.urandom(32))
     return wallet
 
-def find_wallet_with_prefix(prefix):
+# 查找具有特定前缀和后缀的钱包地址的函数
+def find_wallet_with_prefix_and_suffix(prefix, suffix, thread_name):
     counter = 0
     while True:
         counter += 1
         wallet = create_wallet()
         wallet_address = wallet.address
 
-        if wallet_address.startswith(prefix):
+        if wallet_address.startswith(prefix) and wallet_address.endswith(suffix):
             with open('wallets.txt', 'a') as f:
                 f.write(f"Wallet found: Address: {wallet_address} Private key: {wallet._private_key.hex()}\n")
                 print(f"Wallet found: Address: {wallet_address} Private key: {wallet._private_key.hex()}")
-        if counter % 1000 == 0:
-            print(f"Thread {threading.current_thread().name} tried {counter} wallets.")
 
-# 前缀要求
-prefix = '0x000000'
+        if counter % 10000 == 0:
+            print(f"{thread_name} tried {counter} wallets.")
+
+# 定义目标前缀和后缀、线程数
+prefix = '0x00'
+suffix = '00'
 
 # 创建多线程
 thread_count = 8
 threads = []
 
 for i in range(thread_count):
-    t = threading.Thread(target=find_wallet_with_prefix, args=(prefix,), name=f'Thread-{i + 1}')
+    t = threading.Thread(target=find_wallet_with_prefix_and_suffix, args=(prefix, suffix, f'Thread-{i + 1}'), name=f'Thread-{i + 1}')
     threads.append(t)
     t.start()
-    print(i,'线程已启动')
+    print(i, '线程已启动')
+
 # 等待所有线程完成
 for t in threads:
     t.join()
