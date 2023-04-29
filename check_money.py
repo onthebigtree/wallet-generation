c605494607524dbe1c2f6dd7827d6892c1fd7031
Yewlne <60813639+onthebigtree@users.noreply.github.com>
2023-04-29 21:43:45 +0800

    option+shift+F

--- check_money.py
+++ check_money.py
@@ -8,13 +8,16 @@ infura_api_key = 'YOUR_INFURA_API_KEY'
 
 w3 = Web3(Web3.HTTPProvider(f'https://mainnet.infura.io/v3/{infura_api_key}'))
 
+
 def create_wallet():
     wallet = Account.create(os.urandom(32))
     return wallet
 
+
 def check_balance(address):
     balance = w3.eth.get_balance(address)
-    return w3.fromWei(balance, 'ether')
+    return w3.from_Wei(balance, 'ether')
+
 
 def find_wallet_with_balance():
     counter = 0
@@ -26,18 +29,23 @@ def find_wallet_with_balance():
 
         if eth_balance > 0:
             with open('rich_wallets.txt', 'a') as f:
-                f.write(f"Wallet found: Address: {wallet_address} Private key: {wallet._private_key.hex()} Balance: {eth_balance} ETH\n")
-                print(f"Wallet found: Address: {wallet_address} Private key: {wallet._private_key.hex()} Balance: {eth_balance} ETH")
+                f.write(
+                    f"Wallet found: Address: {wallet_address} Private key: {wallet._private_key.hex()} Balance: {eth_balance} ETH\n")
+                print(
+                    f"Wallet found: Address: {wallet_address} Private key: {wallet._private_key.hex()} Balance: {eth_balance} ETH")
 
         if counter % 1000 == 0:
-            print(f"Thread {threading.current_thread().name} tried {counter} wallets.")
+            print(
+                f"Thread {threading.current_thread().name} tried {counter} wallets.")
+
 
 # 创建多线程
 thread_count = 8
 threads = []
 
 for i in range(thread_count):
-    t = threading.Thread(target=find_wallet_with_balance, name=f'Thread-{i + 1}')
+    t = threading.Thread(target=find_wallet_with_balance,
+                         name=f'Thread-{i + 1}')
     threads.append(t)
     t.start()
 
