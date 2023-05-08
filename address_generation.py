8d98df0a67da693617943c90811ceba8e4f66964
Yewlne <60813639+onthebigtree@users.noreply.github.com>
2023-05-08 21:47:09 +0800

    更新钱包创建输入方式

--- address_generation.py
+++ address_generation.py
@@ -2,20 +2,9 @@ import os
 import multiprocessing
 from eth_account import Account
 
-prefix = '0x000' #你想要的前缀
-suffix = '000'  #你想要的后缀
-
 def create_wallet():
     wallet = Account.create(os.urandom(32))
     return wallet
-
-''' 
-安全换速度的方法：十六进制值而不是随机生成值可能会稍微提高速度，使用时请注释掉上面的 create_wallet
-def create_wallet():
-    fixed_hex_value = bytes.fromhex("0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef")
-    wallet = Account.create(fixed_hex_value)
-    return wallet
-'''
 
 def find_wallet_with_prefix_and_suffix(prefix1, suffix1):
     counter = 0
@@ -29,21 +18,21 @@ def find_wallet_with_prefix_and_suffix(prefix1, suffix1):
                 f.write(f"Wallet found: Address: {wallet_address} Private key: {wallet._private_key.hex()}\n")
                 print(f"Wallet found: Address: {wallet_address} Private key: {wallet._private_key.hex()}")
 
-#        if counter % 10000 == 0:  #进度提醒，会影响速度
-#            print(f"Process {multiprocessing.current_process().name} tried {counter} wallets.")
-
-def worker():
+def worker(prefix, suffix):
     find_wallet_with_prefix_and_suffix(prefix, suffix)
 
 if __name__ == '__main__':
+    prefix = input("请输入你想要的以太坊钱包地址前缀：")
+    suffix = input("请输入你想要的以太坊钱包地址后缀：")
+
     print("程序已开始运行。正在使用多进程查找指定前缀和后缀的以太坊钱包地址。请耐心等待。")
 
-#进程数，默认为CPU核心数
+    #进程数，默认为CPU核心数
     process_count = multiprocessing.cpu_count()
     processes = []
 
     for _ in range(process_count):
-        p = multiprocessing.Process(target=worker)
+        p = multiprocessing.Process(target=worker, args=(prefix, suffix))
         processes.append(p)
         p.start()
 
